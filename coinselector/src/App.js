1654b1105c57da8af72ab4e5d3e53f500f73dd43
Getachew Akalu <getachewakalu@Getachews-iMac.local>
2025-07-24 14:44:37 -0700

    App data updated

--- coinselector/src/App.js
+++ coinselector/src/App.js
@@ -1,14 +1,7 @@
-import {useState} from 'react'
+
 import './App.css';
 import Dollar from './Dollar';
-const COINS = [
-  { name: 'Dollar', value: 100 },
-  { name: 'Half Dollar', value: 50 },
-  { name: 'Quarter', value: 25 },
-  { name: 'Dime', value: 10 },
-  { name: 'Nickel', value: 5 },
-  { name: 'Penny', value: 1 },
-];
+
 function App() {
   
   return (
